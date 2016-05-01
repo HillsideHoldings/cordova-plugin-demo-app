142799491bf50559be0710cc3ac7a5027b1966ca
Nicolas Rostov <NicolasRostov@gmail.com>
2016-05-01 15:43:16 +0300

    Lockscreen plugin update

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
+*/
 var app = {
     // Application Constructor
     initialize: function() {
@@ -44,62 +44,116 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-        
-        // Start playback
-        var my_media = new Media('http://www.songspk320z.us/songoftheday/[Songs.PK]%20Khaike%20Paan%20Banaraswala%20-%20Don%20(2006).mp3',
-                                 function () {
-                                    console.log("playAudio():Audio Success");
-                                 },
-                                 // error callback
-                                 function (err) {
-                                    console.log("playAudio():Audio Error: " + err);
-                                 }
-                            );
-        my_media.play();
-        
-        // you can change skip interval (in seconds)
-        var skipInterval = 30;
-        // this value will be shown on fwd/bckwd buttons labels
-        Lockscreen.setSkipInterval(skipInterval);
-        
-        // set track info for lock screen
-        Lockscreen.setTitle('Title');
-        Lockscreen.setSubtitle('Subtitle');
-        // track length is not available right away
-        setTimeout(function() { Lockscreen.setTrackLength(my_media.getDuration()); }, 2000);
-        Lockscreen.setCurrentTime(0);
-        Lockscreen.setArtworkURL('http://www.songspk320z.us/img/globe.png');
-    
-        // here is call backs for user actions
-        Lockscreen.registerActions(function() {
-                                        my_media.play();
-                                   },
-                                   function() {
-                                        my_media.pause();
-                                   },
-                                   function() {
-                                        // TODO: toggle logic
-                                        my_media.play();
-                                   },
-                                   function() {
-                                        my_media.getCurrentPosition(function(p) {
-                                                                        p += skipInterval;
-                                                                        my_media.seekTo(1000*p);
-                                                                        Lockscreen.setCurrentTime(p);
-                                                                    }, null);
-                                   },
-                                   function() {
-                                        my_media.getCurrentPosition(function(p) {
-                                                                        p -= skipInterval;
-                                                                        my_media.seekTo(1000*p);
-                                                                        Lockscreen.setCurrentTime(p);
-                                                                    }, null);
-                                   });
-        
+        document.getElementById("playbtn").addEventListener("click", playAudio);
+        initMedia();
     },
 
 };
 
 app.initialize();
 
+
+var skipInterval = 25;
+var my_media = null;
+var isPlaying = false;
+var lockscreen = null;
+var volumeSlider = null;
+
+function initMedia() {
+	var url = "http://www.songspk320z.us/songoftheday/[Songs.PK]%20Khaike%20Paan%20Banaraswala%20-%20Don%20(2006).mp3";
+	// Play the audio file at url
+	my_media = new Media(url,
+	// success callback
+	function() {
+		console.log("playAudio():Audio Success");
+	},
+	// error callback
+	function(err) {
+		console.log("playAudio():Audio Error: " + err);
+	});
+
+	initLockScreenControls();
+  // let's tell system that our app should not go in pause state.
+	cordova.plugins.backgroundMode.enable();
+}
+
+function initLockScreenControls() {
+	lockscreen = new Lockscreen();
+	lockscreen.setState(Lockscreen.STATE_STOPPED);
+	lockscreen.listenActions(onLockScreenEventReceived);
+  lockscreen.setSkipInterval(skipInterval);
+}
+
+// Play audio
+//
+function playAudio() {
+	if (isPlaying) {
+		// Pause audio
+		my_media.pause();
+		isPlaying = false;
+		document.getElementById("playbtn").innerHTML = "Play";
+		lockscreen.setState(Lockscreen.STATE_PAUSED);
+	} else {
+		// Play audio
+		my_media.play();
+		isPlaying = true;
+		document.getElementById("playbtn").innerHTML = "Pause";
+
+		lockscreen.setState(Lockscreen.STATE_PLAYING);
+    lockscreen.setMetadata({
+      "title" : "Khai k paan banaras wala",
+      "subTitle" : "Don",
+      "image" : "http://www.songspk320z.us/img/globe.png"
+    });
+
+    setTimeout(function() {
+      lockscreen.setMetadata({"duration": my_media.getDuration()} );
+      my_media.getCurrentPosition(function(p) {
+        lockscreen.setCurrentTime(p);
+  		}, null);
+    }, 100);
+
+	}
+}
+
+function onLockScreenEventReceived(event) {
+	if (event == Lockscreen.ACTION_PLAY_PAUSE
+      || event == Lockscreen.ACTION_PLAY
+      || event == Lockscreen.ACTION_PAUSE) {
+		// toggle playing
+		playAudio();
+	} else if (event == Lockscreen.ACTION_REWIND) {
+		my_media.getCurrentPosition(function(p) {
+			p -= skipInterval;
+			my_media.seekTo(1000 * p);
+      lockscreen.setCurrentTime(p);
+		}, null);
+	} else if (event == Lockscreen.ACTION_FORWARD) {
+		my_media.getCurrentPosition(function(p) {
+			p += skipInterval;
+			my_media.seekTo(1000 * p);
+      lockscreen.setCurrentTime(p);
+		}, null);
+	}
+}
+
+function initVolumeSlide() {
+	volumeSlider = window.plugins.volumeSlider;
+	volumeSlider.createVolumeSlider(10, 50, 300, 30); // origin x, origin y, width, height
+	volumeSlider.showVolumeSlider();
+	// lets give some timeout so that volume slider is fully loaded.
+	setTimeout(function(){
+
+		volumeSlider.getVolumeLevel(function(vol){
+			console.log("Getting current volume: "+vol);
+		}, function(err){
+			console.log("Error getting volume: "+err);
+		});
+
+		// Register for event to get volume when user press volume up or down key. Do not forget to unregister this when application is closed.
+		volumeSlider.registerVolumeUpdate(function(vol){
+			console.log("Volume changed: "+vol);
+		});
+
+	}, 500);
+}
--- platforms/ios/www/plugins/cordova-plugin-lockscreen/www/lockscreen.js
+++ platforms/ios/www/plugins/cordova-plugin-lockscreen/www/lockscreen.js
@@ -1,41 +1,48 @@
 cordova.define("cordova-plugin-lockscreen.Lockscreen", function(require, exports, module) {
-               
-module.exports = {
-               setTitle: function(title) {
-                    cordova.exec(null, null, "Lockscreen", "setTitle", [title]);
-               },
-               
-               setSubtitle: function(subtitle) {
-                    cordova.exec(null, null, "Lockscreen", "setSubtitle", [subtitle]);
-               },
-               
-               setTrackLength: function(trackLength) {
-                    cordova.exec(null, null, "Lockscreen", "setTrackLength", [trackLength]);
-               },
-               
-               setCurrentTime: function(currentTime) {
-                    cordova.exec(null, null, "Lockscreen", "setCurrentTime", [currentTime]);
-               },
-               
-               setArtworkURL: function(artworkUrl) {
-                    cordova.exec(null, null, "Lockscreen", "setArtworkURL", [artworkUrl]);
-               },
-               
-               setSkipInterval: function(skipInterval) {
-                    cordova.exec(null, null, "Lockscreen", "setSkipInterval", [skipInterval]);
-               },
-               
-               registerActions: function(playAction, pauseAction, toggleAction, forwardAction, backwardAction) {
-                    cordova.exec(function(message) {
-                                    switch(message) {
-                                        case 1:playAction();break;
-                                        case 2:pauseAction();break;
-                                        case 3:toggleAction();break;
-                                        case 4:backwardAction();break;
-                                        case 5:forwardAction();break;
-                                    }
-                                 },
-                                 null, "Lockscreen", "registerActions", [playAction, pauseAction, toggleAction, forwardAction, backwardAction]);
-               }
-};
+
+		var Lockscreen = function() {
+			cordova.exec(null, null, "Lockscreen", "init", []);
+		};
+
+		// Media states
+		Lockscreen.STATE_PLAYING = "PLAYING";
+		Lockscreen.STATE_PAUSED = "PAUSED";
+		Lockscreen.STATE_STOPPED = "STOPPED";
+		Lockscreen.STATE_STOPPED = "STOPPED";
+		Lockscreen.ACTION_PLAY_PAUSE = "PLAY_PAUSE";
+		Lockscreen.ACTION_PLAY = "PLAY";
+		Lockscreen.ACTION_PAUSE = "PAUSE";
+		Lockscreen.ACTION_REWIND = "REWIND";
+		Lockscreen.ACTION_FORWARD = "FORWARD";
+
+
+		Lockscreen.prototype.setState = function(state) {
+			cordova.exec(null, null, "Lockscreen", "setState", [state]);
+		};
+
+		Lockscreen.prototype.setMetadata = function(metadata) {
+			cordova.exec(null, null, "Lockscreen", "setMetadata", [ metadata ]);
+		};
+
+		Lockscreen.prototype.setCurrentTime = function(currentTime) {
+			cordova.exec(null, null, "Lockscreen", "setCurrentTime", [currentTime]);
+		};
+
+		Lockscreen.prototype.setSkipInterval = function(skipInterval) {
+			cordova.exec(null, null, "Lockscreen", "setSkipInterval", [skipInterval]);
+		};
+
+		Lockscreen.prototype.release = function() {
+			cordova.exec(null, null, "Lockscreen", "release", []);
+		};
+
+		Lockscreen.prototype.listenActions = function(onEventReceived) {
+
+			cordova.exec(function(e){
+				onEventReceived(e)
+			}, null, 'Lockscreen',
+					 'listenActions', []);
+		};
+
+			module.exports = Lockscreen;
 });
